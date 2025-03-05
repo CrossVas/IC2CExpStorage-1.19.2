198404b0429a82c57d81275fd8e47571bd8b4efb
CrossVas <cross.wanted.1.4@gmail.com>
2025-03-05 17:42:02 +0900

    changed name

--- src/main/java/ic2/addons/xpstorage/Refs.java
+++ src/main/java/ic2/addons/xpstorage/Refs.java
@@ -3,5 +3,5 @@ package ic2.addons.xpstorage;
 public class Refs {
 
     public static final String ID = "ic2cexpstorage";
-    public static final String NAME = "IC2C: Experience Storage";
+    public static final String NAME = "IC2C: XP Storage";
 }
